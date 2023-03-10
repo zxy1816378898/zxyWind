7078e68a96f0c1152ee098344461c04230cc085d
zxy1816378898 <79346792+zxywind-9527@users.noreply.github.com>
2023-03-10 15:46:10 +0800

    【feat】 地图修改

--- src/views/Map/index.vue
+++ src/views/Map/index.vue
@@ -13,17 +13,19 @@
     </div>
     <!-- 天气 -->
     <div class="weather">
-      <div class="forecast">{{ forecast }}</div>
-      <div class="temperature">{{ nightTemp }}~{{ dayTemp }}℃</div>
+      <div class="bigBox">
+        <div class="city">{{ city }}</div>
+        <div class="clear ">
+          <div class="forecast">{{ forecast }}</div>
+          <div class="temperature">{{ nightTemp }}~{{ dayTemp }}℃</div>
+        </div>
+      </div>
     </div>
     <!-- 搜索框 -->
-    <div class="search">
+    <div class="search" @click="inputClick">
       <input id="tipinput" :value="inputSearchVal" type="text">
       <img class="searchIMG" :src="searchIMG">
-
     </div>
-    <!-- 当前位置 -->
-
   </div>
 </template>
     
@@ -39,8 +41,8 @@ const forecast = ref('')
 const dayTemp = ref('')
 const nightTemp = ref('')
 const city = ref('')
-const geolocation = ref()
 
+const getlocalcity = ref('')
 /**初始化 */
 const initMap = () => {
   window._AMapSecurityConfig = {
@@ -57,7 +59,8 @@ const initMap = () => {
       'AMap.AutoComplete',//输入提示，提供了根据关键字获得提示信息的功能
       'AMap.Geolocation',//定位，提供了获取用户当前准确位置、所在城市的方法
       'AMap.AdvancedInfoWindow',//高级信息窗体，整合了周边搜索、路线规划功能
-      'AMap.Weather'//天气预报插件，用于获取未来的天气信息
+      'AMap.Weather',//天气预报插件，用于获取未来的天气信息
+      'AMap.CitySearch',//根据IP返回对应城市信息
     ]
   })
     .then((AMap) => {
@@ -66,45 +69,22 @@ const initMap = () => {
         viewMode: '3D', //是否为3D地图模式
         terrain: true,// 开启地形图
         pitch: 50, // 地图俯仰角度，有效范围 0 度- 83 度
-        zoom: 10, //初始化地图级别
+        zoom: 18, //初始化地图级别
         mapStyle: 'amap://styles/normal',
         buildingAnimation: true,  // 楼快出现是否动画
         resizeEnable: false,  // 是否启用大小调整
         rotateEnable: false, // 是否启用旋转
         pitchEnable: true,    // 是否启用倾斜
         expandZoomRange: false,  // 扩大收缩范围
+        resizeEnable: true
       })
-      geolocation.value = new AMap.Geolocation(options);
-
-      var options = {
-        'showButton': true,//是否显示定位按钮
-        'position': 'LB',//定位按钮的位置
-        /* LT LB RT RB */
-        'offset': [10, 20],//定位按钮距离对应角落的距离
-        'showMarker': true,//是否显示定位点
-        'markerOptions': {//自定义定位点样式，同Marker的Options
-          'offset': new AMap.Pixel(-18, -36),
-          'content': '<img :src="localIMG" style="width:36px;height:36px"/>'
-        },
-        'showCircle': true,//是否显示定位精度圈
-        'circleOptions': {//定位精度圈的样式
-          'strokeColor': '#0093FF',
-          'noSelect': true,
-          'strokeOpacity': 0.5,
-          'strokeWeight': 1,
-          'fillColor': '#02B0FF',
-          'fillOpacity': 0.25
-        }
-      }
+      getlocalcity.value = new AMap.CitySearch();
       addselect(AMap)
-      addweather(AMap)
-      addlocaltion(geolocation.value)
+      addlocalCity(getlocalcity.value)
     })
     .catch((e) => {
       console.log(e);
     });
-
-
 };
 
 //输入提示
@@ -123,32 +103,25 @@ const addselect = (AMap) => {
 //天气
 const addweather = (AMap) => {
   let weather = new AMap.Weather();
-  weather.getForecast('淮安市', function (err, data) {
+  weather.getForecast(city.value, function (err, data) {
     if (!err) {
       forecast.value = data.forecasts[0].dayWeather;
       dayTemp.value = data.forecasts[0].dayTemp;
       nightTemp.value = data.forecasts[0].nightTemp;
     }
-
   })
 }
-
-//定位当前位置
-const addlocaltion = (geolocation) => {
-  geolocation.getCityInfo(function (status, result) {
-    if (status == 'complete') {
-      onComplete(result)
-    } else {
-      onError(result)
-    }
+//定位当前城市
+const addlocalCity = (getlocalcity) => {
+  getlocalcity.getLocalCity(function (status, result) {
+    city.value = result.city;
   })
-}// data是具体的定位信息
-function onComplete(data) {
-  city.value = data.city
 }
-function onError(data) {
-  // 定位出错
-}
+// 延迟执行
+setTimeout(() => {
+  addweather(AMap)
+}, 1000)
+
 onMounted(() => {
   initMap();
 });
@@ -273,7 +246,7 @@ onMounted(() => {
 }
 
 .weather {
-  width: 100px;
+  width: 150px;
   height: 50px;
   position: absolute;
   right: 360px;
@@ -283,14 +256,38 @@ onMounted(() => {
   color: #7fffd4;
   font-size: 18px;
 
-  .forecast {
+  .bigBox {
     width: 100%;
-    height: 50%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    .city {
+      width: 50%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center
+    }
+
+    .clear {
+      @extend .city;
+      display: flex;
+      flex-direction: column;
+
+      .forecast {
+        width: 100%;
+        height: 50%;
+      }
+
+      .temperature {
+        @extend .forecast;
+      }
+    }
   }
 
-  .temperature {
-    @extend .forecast;
-  }
+
 }
 
 .search {

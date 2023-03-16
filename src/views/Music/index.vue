4c608c9463d485b49ad9c60272133de93bb4e952
zxy1816378898 <79346792+zxywind-9527@users.noreply.github.com>
2023-03-16 15:12:51 +0800

    【feat】热门推荐框架基本完成

--- src/views/Music/index.vue
+++ src/views/Music/index.vue
@@ -22,7 +22,7 @@
     <div class="main_right">
       <!-- 搜索框 -->
       <div class="main_right_search">
-        <el-input v-model="input" class="w-50 m-2" size="large" placeholder="Please input" />
+        <el-input v-model="input" class="w-50 m-2" size="large" placeholder="请输入歌手、歌名、歌词或专辑" />
       </div>
       <!-- 轮播图 -->
       <div class="main_right_banner">
@@ -33,7 +33,31 @@
         </el-carousel>
       </div>
       <!-- 热门推荐 -->
-      <div class="main_right_hot_recommend"></div>
+      <div class="main_right_hot_recommend">
+
+        <div class="main_right_hot_recommend_title">热门推荐
+          <h2>华语</h2>
+          <h2>流行</h2>
+          <h2>摇滚</h2>
+          <h2>电子</h2>
+          <span></span>
+        </div>
+
+        <div class="main_right_hot_recommend_box">
+
+          <div class="main_right_hot_recommend_li" v-for="item in hot_recommend_liAyy">
+
+            <img class="img" :src="getImage(`${item.pic}`)" />
+
+            <div class="text">{{ item.text }}</div>
+
+            <div class="lable">{{ item.lable }}</div>
+
+          </div>
+
+        </div>
+
+      </div>
       <!-- 新碟上架 -->
       <div class="main_right_new_grounding"></div>
       <!-- 最新MV -->
@@ -78,6 +102,33 @@ const leftAyy = ref([
     name: "我的音乐",
   },
 ])
+const hot_recommend_liAyy = ([
+  {
+    pic: "img1",
+    text: "2023网络超好听流行歌曲推荐(更新快)",
+    lable: "#华语 #流行 #网络歌曲"
+  },
+  {
+    pic: "img2",
+    text: "一直不喜欢，纷纷前来梨园踏青观景",
+    lable: "#华语 #流行 #网络歌曲"
+  },
+  {
+    pic: "img3",
+    text: "送给每个因为爱情向现实抗争过的人",
+    lable: "#华语 #流行 #网络歌曲"
+  },
+  {
+    pic: "img4",
+    text: "【日系】当古典的交响乐遇上鲜活的ACG",
+    lable: "#华语 #流行 #网络歌曲"
+  },
+  {
+    pic: "img5",
+    text: "没有动态的日子，都在认真生活",
+    lable: "#华语 #流行 #网络歌曲"
+  },
+])
 function getImage(name) {
   //动态引入图片
   return new URL(`../../assets/images/music/${name}.png`, import.meta.url).href;
@@ -94,6 +145,7 @@ function getImage(name) {
   justify-content: space-around;
   align-items: center;
   box-sizing: border-box;
+
 
   .main_left {
     width: 300px;
@@ -186,6 +238,100 @@ function getImage(name) {
       width: 100%;
       height: 420px;
       border: 1px solid red;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+
+      .main_right_hot_recommend_title {
+        width: 35%;
+        height: 50px;
+        font-size: 28px;
+        font-weight: 700;
+        line-height: 50px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        color: aliceblue;
+
+        h2 {
+          font-weight: 500;
+          font-size: 18px;
+        }
+
+        h2:hover {
+          color: #0FF;
+        }
+
+        & span {
+          width: 115px;
+          height: 12px;
+          position: absolute;
+          background: #0FF;
+          opacity: 0.6;
+          left: 487px;
+          top: 380px;
+          z-index: -1;
+        }
+      }
+
+      .main_right_hot_recommend_box {
+        width: 100%;
+        height: 340px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+
+        .main_right_hot_recommend_li {
+          width: 260px;
+          height: 100%;
+          @include bbl;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+
+          .img {
+            position: relative;
+            width: 200px;
+            height: 200px;
+            border-radius: 8px;
+            top: 25px;
+          }
+
+          .text {
+            width: 90%;
+            font-size: 18px;
+            height: 60px;
+            line-height: 30px;
+            text-align: left;
+            font-weight: 400;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+            word-break: break-all;
+            position: relative;
+            top: 40px;
+          }
+
+          .lable {
+            width: 90%;
+            height: 30px;
+            font-size: 14px;
+            position: relative;
+            top: 50px;
+            color: #0FF;
+            text-align: left;
+
+          }
+
+          &:hover {
+            transform: scale(1.02);
+            transition: 0.5s;
+            @include bbl;
+          }
+        }
+      }
     }
 
     .main_right_new_grounding {

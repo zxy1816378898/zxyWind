c2ca1edb3b90b30cb250c45a6b73add2407b44fe
zxy <1816378898@qq.com>
2024-05-30 10:01:24 +0800

    更新key

--- src/views/Map/GeneralMap/index.vue
+++ src/views/Map/GeneralMap/index.vue
@@ -15,7 +15,7 @@
     <div class="weather">
       <div class="bigBox">
         <div class="city">{{ city }}</div>
-        <div class="clear ">
+        <div class="clear">
           <div class="forecast">{{ forecast }}</div>
           <div class="temperature">{{ nightTemp }}~{{ dayTemp }}℃</div>
         </div>
@@ -23,63 +23,62 @@
     </div>
     <!-- 搜索框 -->
     <div class="search" @click="inputClick">
-      <input id="tipinput" :value="inputSearchVal" type="text">
-    
+      <input id="tipinput" :value="inputSearchVal" type="text" />
     </div>
   </div>
 </template>
-    
-<script setup>
-import htbutt from "@/components/button";
-import AMapLoader from '@amap/amap-jsapi-loader';
 
+<script setup>
+import htbutt from '@/components/button';
+import AMapLoader from '@amap/amap-jsapi-loader';
 
 const map = ref(null);
 const inputSearchVal = ref('');
-const forecast = ref('')
-const dayTemp = ref('')
-const nightTemp = ref('')
-const city = ref('')
+const forecast = ref('');
+const dayTemp = ref('');
+const nightTemp = ref('');
+const city = ref('');
 
-const getlocalcity = ref('')
+const getlocalcity = ref('');
 /**初始化 */
 const initMap = () => {
   window._AMapSecurityConfig = {
     securityJsCode: '70ad83856e431e14d13ce6124ff5f117',
   };
   AMapLoader.load({
-    key: '38fd1ab532e5a8c3a0da71b76e67c78e',
+    key: '988db99544ff93fc3b31aeff474e0d58',
     version: '2.0',
     plugins: [
-      'AMap.ToolBar',//工具条，控制地图的缩放、平移等
-      'AMap.Driving',//驾车路线规划服务，提供按照起、终点进行驾车路线的功能
-      'AMap.ControlBar',//组合了旋转、倾斜、复位在内的地图控件。
-      'AMap.PlaceSearch',//地点搜索服务，提供了关键字搜索、周边搜索、范围内搜索等功能
-      'AMap.AutoComplete',//输入提示，提供了根据关键字获得提示信息的功能
-      'AMap.Geolocation',//定位，提供了获取用户当前准确位置、所在城市的方法
-      'AMap.AdvancedInfoWindow',//高级信息窗体，整合了周边搜索、路线规划功能
-      'AMap.Weather',//天气预报插件，用于获取未来的天气信息
-      'AMap.CitySearch',//根据IP返回对应城市信息
-    ]
+      'AMap.ToolBar', //工具条，控制地图的缩放、平移等
+      'AMap.Driving', //驾车路线规划服务，提供按照起、终点进行驾车路线的功能
+      'AMap.ControlBar', //组合了旋转、倾斜、复位在内的地图控件。
+      'AMap.PlaceSearch', //地点搜索服务，提供了关键字搜索、周边搜索、范围内搜索等功能
+      'AMap.AutoComplete', //输入提示，提供了根据关键字获得提示信息的功能
+      'AMap.Geolocation', //定位，提供了获取用户当前准确位置、所在城市的方法
+      'AMap.AdvancedInfoWindow', //高级信息窗体，整合了周边搜索、路线规划功能
+      'AMap.Weather', //天气预报插件，用于获取未来的天气信息
+      'AMap.CitySearch', //根据IP返回对应城市信息
+    ],
   })
     .then((AMap) => {
-      map.value = new AMap.Map('container', {     //设置地图容器id
-        pitch: 5,// 地图俯仰角度，有效范围 0 度- 83 度
+      map.value = new AMap.Map('container', {
+        //设置地图容器id
+        pitch: 5, // 地图俯仰角度，有效范围 0 度- 83 度
         viewMode: '3D', //是否为3D地图模式
-        terrain: true,// 开启地形图
+        terrain: true, // 开启地形图
         pitch: 50, // 地图俯仰角度，有效范围 0 度- 83 度
         zoom: 18, //初始化地图级别
         mapStyle: 'amap://styles/normal',
-        buildingAnimation: true,  // 楼快出现是否动画
-        resizeEnable: false,  // 是否启用大小调整
+        buildingAnimation: true, // 楼快出现是否动画
+        resizeEnable: false, // 是否启用大小调整
         rotateEnable: false, // 是否启用旋转
-        pitchEnable: true,    // 是否启用倾斜
-        expandZoomRange: false,  // 扩大收缩范围
-        resizeEnable: true
-      })
+        pitchEnable: true, // 是否启用倾斜
+        expandZoomRange: false, // 扩大收缩范围
+        resizeEnable: true,
+      });
       getlocalcity.value = new AMap.CitySearch();
-      addselect(AMap)
-      addlocalCity(getlocalcity.value)
+      addselect(AMap);
+      addlocalCity(getlocalcity.value);
     })
     .catch((e) => {
       console.log(e);
@@ -88,17 +87,17 @@ const initMap = () => {
 
 //输入提示
 const addselect = (AMap) => {
-  let autoOptions = { input: "tipinput" };
+  let autoOptions = { input: 'tipinput' };
   let auto = new AMap.AutoComplete(autoOptions);
   let placeSearch = new AMap.PlaceSearch({
-    map: map.value
-  });  //构造地点查询类
-  auto.on("select", select);//注册监听，当选中某条记录时会触发
+    map: map.value,
+  }); //构造地点查询类
+  auto.on('select', select); //注册监听，当选中某条记录时会触发
   function select(e) {
     placeSearch.setCity(e.poi.adcode);
-    placeSearch.search(e.poi.name);  //关键字查询查询
+    placeSearch.search(e.poi.name); //关键字查询查询
   }
-}
+};
 //天气
 const addweather = (AMap) => {
   let weather = new AMap.Weather();
@@ -108,25 +107,24 @@ const addweather = (AMap) => {
       dayTemp.value = data.forecasts[0].dayTemp;
       nightTemp.value = data.forecasts[0].nightTemp;
     }
-  })
-}
+  });
+};
 //定位当前城市
 const addlocalCity = (getlocalcity) => {
   getlocalcity.getLocalCity(function (status, result) {
     city.value = result.city;
-  })
-}
+  });
+};
 // 延迟执行
 setTimeout(() => {
-  addweather(AMap)
-}, 1000)
+  addweather(AMap);
+}, 1000);
 
 onMounted(() => {
   initMap();
 });
-
 </script>
-    
+
 <style lang="scss" scoped>
 .main-box {
   width: 100%;
@@ -161,7 +159,7 @@ onMounted(() => {
 }
 
 .box :nth-of-type(1) {
-  background: linear-gradient(to right, transparent, #0FF);
+  background: linear-gradient(to right, transparent, #0ff);
   position: absolute;
   top: 0;
   left: 0;
@@ -172,13 +170,13 @@ onMounted(() => {
 
 .box :nth-of-type(2) {
   position: absolute;
-  background: linear-gradient(to bottom, transparent, #0FF);
+  background: linear-gradient(to bottom, transparent, #0ff);
   top: 0;
   right: 0;
   width: 1px;
   height: 100%;
   animation: run2 1s linear infinite;
-  animation-delay: .5s;
+  animation-delay: 0.5s;
 }
 
 .box :nth-of-type(3) {
@@ -187,7 +185,7 @@ onMounted(() => {
   left: 0;
   width: 100%;
   height: 1px;
-  background: linear-gradient(to left, transparent, #0FF);
+  background: linear-gradient(to left, transparent, #0ff);
   animation: run3 1s linear infinite;
   animation-delay: 1s;
 }
@@ -198,7 +196,7 @@ onMounted(() => {
   left: 0;
   width: 1px;
   height: 100%;
-  background: linear-gradient(to top, transparent, #0FF);
+  background: linear-gradient(to top, transparent, #0ff);
   animation: run4 1s linear infinite;
   animation-delay: 1.5s;
 }
@@ -241,7 +239,6 @@ onMounted(() => {
   100% {
     transform: translateY(-40px);
   }
-
 }
 
 .weather {
@@ -267,7 +264,7 @@ onMounted(() => {
       height: 100%;
       display: flex;
       align-items: center;
-      justify-content: center
+      justify-content: center;
     }
 
     .clear {
@@ -285,8 +282,6 @@ onMounted(() => {
       }
     }
   }
-
-
 }
 
 .search {
@@ -322,9 +317,8 @@ onMounted(() => {
   }
 
   // 去除input框点击时候的黑线，type是什么类型就去除什么类型
-  input[type=text]:focus {
+  input[type='text']:focus {
     outline: none;
   }
 }
 </style>
-    
